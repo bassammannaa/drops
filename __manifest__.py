09351a2a7d9dc86c9a5f14fa4190e6d50018fb15
MedhatMannaa <m_mannaa2008@yahoo.com>
2020-03-10 21:33:27 +0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
         Long description of module's purpose
     """,
 
-    'author': "Medhat Mannaa",
+    'author': "Medhat Mannaa1",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
